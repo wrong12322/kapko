b7e09c22dca391c21339555d2a9ffc721c8015b8
LAB316 Student07 <LAB316_Student07@gau.local>
2019-09-23 18:22:46 +0400

    First Commit

---
+++ week_1/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Aq yvelani yleebi vart datos garda");
+        //int s[] = new int[4];
+        //int a1[] = {1,2,3,4};
+        //for(int i=0; i<a1.length; i++){
+        //    System.out.println(a1[i]);
+        //}
+        //System.out.println("xuti da dedistyvna");
+
+        Random random = new Random();
+        System.out.Println(random.nextInt(100));
+        String M;
+        Scanner input = new Scanner(System.in);
+
+        M = input.nextline();
+        System.out.println(M);
+    }
+}
